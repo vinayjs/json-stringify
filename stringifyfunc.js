a0374226394abae8aa4a44e4c6b4d0488b073cfa
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-08-30 21:55:21 +0530

    array parser

--- stringifyfunc.js
+++ stringifyfunc.js
@@ -1,57 +1,57 @@
-// null parser
+// // null parser
 
-const stringifyNull = (value) => {
-    if (value === null) {
-        return `"${value}"`;
-    }
-    else{
-        return value;
-    }
-}
+// const stringifyNull = (value) => {
+//     if (value === null) {
+//         return `"${value}"`;
+//     }
+//     else{
+//         return value;
+//     }
+// }
 
-console.log(stringifyNull(null));
+// console.log(stringifyNull(null));
 
-// boolean parser
+// // boolean parser
 
-const stringifyBoolean = (value) => {
-    if (value === true) {
-        return `"${true}"`;
-    }
-    else if (value === false) {
-        return `"${false}"`;
-    }
-}
-console.log(stringifyBoolean(true))
-console.log(stringifyBoolean(false))
+// const stringifyBoolean = (value) => {
+//     if (value === true) {
+//         return `"${true}"`;
+//     }
+//     else if (value === false) {
+//         return `"${false}"`;
+//     }
+// }
+// console.log(stringifyBoolean(true))
+// console.log(stringifyBoolean(false))
 
-// number parser
-const stringifyNumber = (value) => {
-    let result = ""
-    if (typeof value === "number") {
-        result += `${value}`
-    }
-    return result;
-}
+// // number parser
+// const stringifyNumber = (value) => {
+//     let result = ""
+//     if (typeof value === "number") {
+//         result += `${value}`
+//     }
+//     return result;
+// }
 
-console.log(stringifyNumber(23));
+// console.log(stringifyNumber(23));
 
-// string parser
+// // string parser
 
-const stringifystring = (value) => {
-    let result = ""
-    if (typeof value === "string") {
-        result += `"${value}"`
-    }
-    return result;
-}
-console.log(stringifystring("vinay"));
+// const stringifystring = (value) => {
+//     let result = ""
+//     if (typeof value === "string") {
+//         result += `"${value}"`
+//     }
+//     return result;
+// }
+// console.log(stringifystring("vinay"));
 
 // array parser
 
 const stringifyArray = (value) => {
     let result = "";
     let lastIndex = value.length - 1;
-    result += '['
+    result += "'["
     for (let i = 0; i < value.length; i++) {
         if (value[i] === null) {
             result += `${value[i]}`
@@ -72,7 +72,7 @@ const stringifyArray = (value) => {
         result += "," 
         } 
     }
-    result += ']'
+    result += "]'"
     return result;
 }
 console.log(stringifyArray(["vinay", 12, null]))
