cab8144e1e0492d5fa01a7bfd579bd67408642d3
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-08-31 20:11:10 +0530

    stringify

--- stringifyfunc.js
+++ stringifyfunc.js
@@ -266,13 +266,13 @@ const stringify = (input) => {
                         output += `"${value}"`
                     }
                     else if (typeof value === "object") {
-                        output += `${stringify(value)}`
+                        output += `${stringifyObject(value)}`
                     }
                     if (key !== lastKey) {
                         output += "," 
                     } 
-                    output += "}"
                 }
+                output += "}"
                 result += output;
             }
             if (value[i] !== value[lastIndex]) {
@@ -305,8 +305,8 @@ const stringify = (input) => {
             else if (typeof value === "string") {
                 result += `"${value}"`
             }
-            else if (typeof value === "array") {
-                result += `${stringifyArray(value)}`
+            else if (Array.isArray(value)) {
+                result += `"${stringifyArray(value)}"`
             }
             else if (typeof value === "object") {
                 result += `${stringifyObject(value)}`
@@ -317,7 +317,7 @@ const stringify = (input) => {
         }
         result += "}'"
         return result;
-    } 
+    }
     let result = ""
     if (input === null) {
        result += stringifyNull(input)
@@ -398,56 +398,56 @@ const stringify = (input) => {
 // console.log(stringify(32));
 // console.log(stringify("vinay", "adarsh"));
 // console.log(stringify([1, 2, 3]));
-// console.log(stringify({"apiVersion":"2.0",
-// "data":{
-//    "updated":"2010-01-07T19:58:42.949Z",
-//    "totalItems":800,
-//    "startIndex":1,
-//    "itemsPerPage":1,
-//    "items":[
-//        {"id": 2000134987,
+console.log(stringify({"apiVersion":"2.0",
+"data":{
+   "updated":"2010-01-07T19:58:42.949Z",
+   "totalItems":800,
+   "startIndex":1,
+   "itemsPerPage":1,
+   "items":[
+       {"id": 2000134987,
 
-//         "uploaded":"2007-06-05T22:07:03.000Z",
-//         "updated":"2010-01-07T13:26:50.000Z",
-//         "uploader":"GoogleDeveloperDay",
-//         "category":"News",
-//         "description":"Google Maps API Introduction ...",
-//         "tags":[
-//            "GDD07","GDD07US","Maps"
-//         ],
-//         "thumbnail":{
-//            "default":"http://i.ytimg.com/vi/hYB0mn5zh2c/default.jpg",
-//            "hqDefault":"http://i.ytimg.com/vi/hYB0mn5zh2c/hqdefault.jpg"
-//         },
-//         "player":{
-//            "default":"http://www.youtube.com/watch?vu003dhYB0mn5zh2c"
-//         },
-//         "content":{
-//            "1":"rtsp://v5.cache3.c.youtube.com/CiILENy.../0/0/0/video.3gp",
-//            "5":"http://www.youtube.com/v/hYB0mn5zh2c?f...",
-//            "6":"rtsp://v1.cache1.c.youtube.com/CiILENy.../0/0/0/video.3gp"
-//         },
-//         "duration":2840,
-//         "aspectRatio":"widescreen",
-//         "rating":4.63,
-//         "ratingCount":68,
-//         "viewCount":220101,
-//         "favoriteCount":201,
-//         "commentCount":22,
-//         "status":{
-//            "value":"restricted",
-//            "reason":"limitedSyndication"
-//         },
-//         "accessControl":{
-//            "syndicate":"allowed",
-//            "commentVote":"allowed",
-//            "rate":"allowed",
-//            "list":"allowed",
-//            "comment":"allowed",
-//            "embed":"allowed",
-//            "videoRespond":"moderated"
-//         }
-//        }
-//    ]
-// }
-// }));
+        "uploaded":"2007-06-05T22:07:03.000Z",
+        "updated":"2010-01-07T13:26:50.000Z",
+        "uploader":"GoogleDeveloperDay",
+        "category":"News",
+        "description":"Google Maps API Introduction ...",
+        "tags":[
+           "GDD07","GDD07US","Maps"
+        ],
+        "thumbnail":{
+           "default":"http://i.ytimg.com/vi/hYB0mn5zh2c/default.jpg",
+           "hqDefault":"http://i.ytimg.com/vi/hYB0mn5zh2c/hqdefault.jpg"
+        },
+        "player":{
+           "default":"http://www.youtube.com/watch?vu003dhYB0mn5zh2c"
+        },
+        "content":{
+           "1":"rtsp://v5.cache3.c.youtube.com/CiILENy.../0/0/0/video.3gp",
+           "5":"http://www.youtube.com/v/hYB0mn5zh2c?f...",
+           "6":"rtsp://v1.cache1.c.youtube.com/CiILENy.../0/0/0/video.3gp"
+        },
+        "duration":2840,
+        "aspectRatio":"widescreen",
+        "rating":4.63,
+        "ratingCount":68,
+        "viewCount":220101,
+        "favoriteCount":201,
+        "commentCount":22,
+        "status":{
+           "value":"restricted",
+           "reason":"limitedSyndication"
+        },
+        "accessControl":{
+           "syndicate":"allowed",
+           "commentVote":"allowed",
+           "rate":"allowed",
+           "list":"allowed",
+           "comment":"allowed",
+           "embed":"allowed",
+           "videoRespond":"moderated"
+        }
+       }
+   ]
+}
+}));
